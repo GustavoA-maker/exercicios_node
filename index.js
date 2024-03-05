0033d63c71ffced966691b83873e6feb7e142fb5
Gustavo <aguirresgustavo2@gmail.com>
2024-03-05 11:36:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// comando para exibir mensagem
+console.log("INTER CAMPEÃO");
+
+// exercicio de calculos
+
+const num1 = 20;
+const num2 = 654;
+
+//soma
+let soma = num1 + num2;
+
+let soma2 = num1 - num2;
+
+let soma3 = num1 / num2;
+
+let soma4 = num1 * num2;
+
+console.log("O valor da soma é " + soma + " - SUCESSO TOTAL");
+console.log("O valor da subtração é " + soma2 + " - SUCESSO TOTAL");
+console.log("O valor da divisão é " + soma3 + " - SUCESSO TOTAL");
+console.log("O valor da multiplicação é " + soma4 + " - SUCESSO TOTAL");
